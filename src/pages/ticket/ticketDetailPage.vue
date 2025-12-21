df24171957992bea54d1c2fdacfd17ecb90973ac
anechaBoon <82567055+anechaboon@users.noreply.github.com>
2025-12-21 12:41:37 +0700

    doing show ticket detail

--- src/pages/ticket/ticketDetailPage.vue
+++ src/pages/ticket/ticketDetailPage.vue
@@ -2,127 +2,194 @@
     <v-form ref="form" v-model="valid">
         <v-container>
             <v-row>
-                <v-col cols="12" md="5">
-                    <v-card outlined class="pa-2 mb-4 pb-5">
-                        <v-row class="px-3 py-1 mt-0">
-                            <v-col cols="12" md="7" class="py-1">
-                                <h3>Ticket</h3>
+                <v-col cols="12" md="6">
+                    <v-card class="pa-4 ticket-card" rounded="lg" elevation="2">
+                        <v-row>
+                            <!-- LEFT -->
+                            <v-col cols="12" md="9">
+                                <div class="d-flex align-center justify-space-between mb-2">
+                                    <div>
+                                        <h3 class="">Ticket</h3>
+                                        <div class="text-body-2 text-grey">
+                                            {{ ticket.title }}
+                                            <v-btn
+                                                size="x-small"
+                                                icon
+                                                variant="text"
+                                                class="ml-1"
+                                            >
+                                                <v-icon size="14">ri-pencil-line</v-icon>
+                                            </v-btn>
+                                        </div>
+                                    </div>
+
+                                    <v-btn icon variant="text">
+                                        <v-icon class="text-h5">ri-more-2-line</v-icon>
+                                    </v-btn>
+                                </div>
+
+
+                                <v-row class="text-body-2 pt-3">
+                                    <v-col cols="2" class="py-3">
+                                        <strong>Ticket Id:</strong>
+                                    </v-col>
+                                    <v-col cols="10" class="py-3">
+                                        <span class="text-warning ml-1">#{{ ticket.id }}</span>
+                                    </v-col>
+
+                                </v-row>
+
+                                <v-row class="text-body-2 mt-0">
+                                    <v-col cols="2" class="py-3">
+                                        <strong>Created:</strong>
+                                    </v-col>
+                                    <v-col cols="10" class="py-3">
+                                        <span class="ml-1">{{ moment(ticket.created_at).format('Y-m-d H:m') }}</span>
+                                    </v-col>
+                                </v-row>
+
+                                <v-row class="text-body-2  mt-0">
+                                    <v-col cols="2" class="py-3">
+                                        <strong>Contact:</strong>
+                                    </v-col>
+                                    <v-col cols="10" class="py-3">
+                                        <div class="ml-1 text-body-2 text-grey">
+                                            {{ ticket.contact_email }}
+                                            <v-btn
+                                                size="x-small"
+                                                icon
+                                                variant="text"
+                                                class="ml-1"
+                                            >
+                                                <v-icon size="14">ri-pencil-line</v-icon>
+                                            </v-btn>
+                                        </div>
+                                    </v-col>
+                                </v-row>
+
+                                <v-row class="text-body-2 mt-0" align="start">
+                                    <!-- Label -->
+                                    <v-col cols="12" md="2" class="py-3">
+                                        <strong>Cc Email:</strong>
+                                    </v-col>
+
+                                    <v-col cols="12" md="10" class="py-3">
+                                        <div class="d-flex flex-column">
+                                        <template v-for="(email, index) in ticket.cc_emails" :key="index">
+                                            <div class="d-flex align-center mb-1">
+                                            <v-chip
+                                                size="small"
+                                                color="primary"
+                                                variant="tonal"
+                                            >
+                                                {{ email.cc_email }}
+                                            </v-chip>
+                                            <v-btn
+                                                size="small"
+                                                variant="text"
+                                                class="my-0 mx-0 px-1 text-none"
+                                                @click="ticket.cc_emails.splice(index, 1)"
+                                            >
+                                                (remove)
+                                            </v-btn>
+                                            <v-btn
+                                                v-if="index === 0"
+                                                size="x-small"
+                                                icon
+                                                variant="text"
+                                                class="ml-1"
+                                            >
+                                                <v-icon size="14">ri-add-circle-line</v-icon>
+                                            </v-btn>
+                                            </div>
+                                        </template>
+                                        </div>
+                                    </v-col>
+                                </v-row>
+
+
+                                <p class="text-body-2 text-grey mt-3">
+                                {{ ticket.description }}
+                                </p>
                             </v-col>
-                        </v-row>
-                        <!-- Ticket && Priority -->
-                        <v-row class="px-3 py-1 mt-0">
-                            <v-col cols="12" md="7">
-                                <div class="d-flex align-center pl-1">
-                                    <!-- Title -->
-                                    <p class="mb-0">{{ ticket.title }}</p>
-                                    <i class="ri-pencil-line ml-2"></i>
+
+                            <!-- RIGHT -->
+                            <v-col cols="12" md="3" class="d-flex flex-column align-center right-panel">
+
+                                <div class="mb-6 d-flex align-center w-100">
+                                    <v-spacer />
+                                    <v-icon size="24">ri-coupon-2-line</v-icon>
+                                </div>
+
+                                <div class="text-center mb-6 align-center">
+                                    <div class="d-flex align-center">
+                                        <div class="text-error text-h5 font-weight-bold float-start">
+                                            {{ ticket.priority }}
+                                        </div>
+                                    </div>
+                                    <div class="d-flex">
+                                        <div class="text-caption text-grey">
+                                            ticket priority
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="text-center mb-6 mt-auto">
+                                    <div class="d-flex align-center">
+                                        <div class="font-weight-bold float-start">
+                                            11h 30min
+                                        </div>
+                                    </div>
+                                    <div class="d-flex">
+                                        <div class="text-caption text-grey">
+                                            time remaining
+                                        </div>
+                                    </div>
                                 </div>
                             </v-col>
-                            <v-col cols="12" md="5" class="px-1">
-                                <!-- Priority -->
-                                <v-select
-                                    v-model="ticket.priority"
-                                    :items="globalPriorityOptions"
-                                    label="Priority"
-                                ></v-select>
-                            </v-col>
-                        </v-row>
-                        <!-- Email -->
-                        <v-row class="px-3 py-1 mt-0">
-                            <v-col cols="12" md="7" class="px-1">
-                                <!-- Email -->
-                                <v-text-field
-                                    v-model="ticket.contact_email"
-                                    label="Contact Email"
-                                    type="email"
-                                    autocomplete="email"
-                                />
-                            </v-col>
-                        </v-row>
-                        <!-- CC-Emails -->
-                        <template v-for="(cc_email, index) in ticket.cc_emails" :key="index">
-                            <v-row class="py-1 mt-0" >
-                                <v-col cols="10" md="7" class="px-1">
-                                    <!-- CC-Emails -->
-                                    <v-text-field
-                                        v-model="cc_email.cc_email"
-                                        :label="`CC Email ${index + 1}`"
-                                        type="email"
-                                        autocomplete="new-email"
-                                        class="ml-3"
-                                    />
-                                </v-col>
 
-                                <v-col cols="2" md="5" class="px-1">
-                                    <!-- btn add more cc email -->
-                                    <v-btn
-                                        v-if="index === 0"
-                                        icon="ri-add-line"
-                                        size="small"
-                                        variant="text"
-                                        @click="ticket.cc_emails.push('')"
-                                    />
-                                    <v-btn
-                                        v-if="index > 0"
-                                        icon="ri-subtract-line"
-                                        size="small"
-                                        variant="text"
-                                        color="error"
-                                        @click="ticket.cc_emails.splice(index, 1)"
-                                    />
-
-                                </v-col>
-                            </v-row>
-                        </template>
-                        <!-- Description -->
-                        <v-row class="px-3 py-1 mt-0">
-                            <v-col cols="12" md="7" class="px-1">
-                                <v-textarea
-                                    v-model="ticket.description"
-                                    rows="3"
-                                    label="Description"
-                                ></v-textarea>
-                                
-                            </v-col>
                         </v-row>
                     </v-card>
                 </v-col>
-                <v-col cols="12" md="4">
-                    <v-card outlined class="pa-2 mb-4 pb-5" min-height="377">
-                        <v-row class="px-3 py-1 mt-0">
+                <v-col cols="12" md="3">
+                    <v-card outlined class="pa-2 mb-4 pb-5" min-height="335">
+                        <v-row class="px-3 py-1 mt-0 align-center mb-1">
                             <v-col cols="12" md="7" class="px-1 py-1">
                                 <h3>Organization</h3>
+                            </v-col>
+                            <v-col cols="12" md="5" class="px-1 py-1 text-right">
+                                <v-icon size="24">ri-anchor-line</v-icon>
                             </v-col>
                         </v-row>
                         <!-- Organization -->
                         <v-row class="px-3 py-1 mt-0">
-                            <v-col cols="12" md="12" class="px-1">
+                            <v-col cols="12" md="12" class="px-1 py-2">
                                 <!-- Organization -->
+                                <v-label class="mb-1">Organization</v-label>
                                 <v-select
                                     v-model="ticket.organization.name"
                                     :items="organizationOptions"
-                                    label="Organization"
                                 ></v-select>
                             </v-col>
                         </v-row>
                         <!-- Vessel -->
                         <v-row class="px-3 py-1 mt-0">
-                            <v-col cols="12" md="12" class="px-1">
+                            <v-col cols="12" md="12" class="px-1 py-2">
                                 <!-- Vessel -->
+                                <v-label class="mb-1">Vessel</v-label>
                                 <v-select
                                     v-model="ticket.vessel.name"
                                     :items="vesselOptions"
-                                    label="Vessel"
                                 ></v-select>
                             </v-col>
                         </v-row>
                         <!-- Assigned To -->
                         <v-row class="px-3 py-1 mt-0">
-                            <v-col cols="12" md="12" class="px-1">
+                            <v-col cols="12" md="12" class="px-1 py-2">
+                                <v-label class="mb-1">Assigned To</v-label>
                                 <v-select
                                     v-model="ticket.assigned_to_user_name"
                                     :items="userOptions"
-                                    label="Assigned To"
                                 ></v-select>
                                 
                             </v-col>
@@ -130,41 +197,44 @@
                     </v-card>
                 </v-col>
                 <v-col cols="12" md="3">
-                    <v-card outlined class="pa-2 mb-4 pb-5" min-height="377">
-                        <v-row class="px-3 py-1 mt-0">
+                    <v-card outlined class="pa-2 mb-4 pb-5" min-height="335">
+                        <v-row class="px-3 py-1 mt-0 align-center mb-1">
                             <v-col cols="12" md="7" class="px-1 py-1">
                                 <h3>Category</h3>
+                            </v-col>
+                            <v-col cols="12" md="5" class="px-1 py-1 text-right">
+                                <v-icon size="24">ri-git-repository-private-fill</v-icon>
                             </v-col>
                         </v-row>
                         <!-- Category -->
                         <v-row class="px-3 py-1 mt-0">
-                            <v-col cols="12" md="12" class="px-1">
+                            <v-col cols="12" md="12" class="px-1 py-2">
                                 <!-- Category -->
+                                <v-label class="mb-1">Category</v-label>
                                 <v-select
                                     v-model="ticket.category.name"
                                     :items="categoryOptions"
-                                    label="Category"
                                 ></v-select>
                             </v-col>
                         </v-row>
                         <!-- Status -->
                         <v-row class="px-3 py-1 mt-0">
-                            <v-col cols="12" md="12" class="px-1">
+                            <v-col cols="12" md="12" class="px-1 py-2">
                                 <!-- Status -->
+                                <v-label class="mb-1">Status</v-label>
                                 <v-select
                                     v-model="ticket.status"
                                     :items="ticketStatusOptions"
-                                    label="Status"
                                 ></v-select>
                             </v-col>
                         </v-row>
                         <!-- Service Line -->
                         <v-row class="px-3 py-1 mt-0">
-                            <v-col cols="12" md="12" class="px-1">
+                            <v-col cols="12" md="12" class="px-1 py-2">
+                                <v-label class="mb-1">Service Line</v-label>
                                 <v-select
                                     v-model="ticket.service_line.name"
                                     :items="serviceLineOptions"
-                                    label="Service Line"
                                 ></v-select>
                                 
                             </v-col>
@@ -180,7 +250,9 @@
 
 <script setup>
 import { getTicket } from '@/api/ticket.api';
+import moment from 'moment';
 import { onMounted, ref } from 'vue';
+
 
 const ticket = ref({
     title: '',

96aa12f734c1ddfbe65c71d6e9e6b3b5085b8920
anechaBoon <82567055+anechaboon@users.noreply.github.com>
2025-12-22 10:38:55 +0700

    change enum priority

--- src/constants/global.js
+++ src/constants/global.js
@@ -15,14 +15,12 @@ const GLOBAL_PRIORITIES = Object.freeze({
     low: 'Low',
     medium: 'Medium',
     high: 'High',
-    urgent: 'Urgent',
 })
 
 const PRIORITY_COLORS = Object.freeze({
     low: 'green',
     medium: 'blue',
     high: 'red',
-    urgent: '#e111e1',
 })
 
 export { GLOBAL_PRIORITIES, GLOBAL_STATUS, PRIORITY_COLORS, TICKET_STATUS };
